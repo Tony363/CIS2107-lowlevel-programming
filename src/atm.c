9d8390afff6740167ba6e00ca26a00607ab3e76f
tony <thefool363@gmail.com>
2023-09-13 17:34:26 -0400

    unsigned ints

--- src/atm.c
+++ src/atm.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <math.h>
 
-int validate_pin(double frac_part,int input){
+int validate_pin(double frac_part,unsigned int input){
     return (input == 3014);
 }
 
-int validate_menu(double frac_part,int input){
+int validate_menu(double frac_part,unsigned int input){
     return (frac_part == 0 && input > 0 && input < 5);
 }
 
-int validate_withrawal(double frac_part,int input){
+int validate_withrawal(double frac_part,unsigned int input){
     return (frac_part == 0 && input > 0 && input < 1000 && input % 20 == 0);
 };
 
-int validate_deposit(double frac_part,int input){
+int validate_deposit(double frac_part,unsigned int input){
     return (frac_part == 0 && input > 0 && input < 10000);
 }
 
-double prompt_input(char* prompt_message, int (*return_check)(double,int)){
+double prompt_input(char* prompt_message, int (*return_check)(double,unsigned int)){
     double frac_part,int_part,input = 0.0;
     int prompts_num = 0;
     printf("%s",prompt_message);
@@ -26,10 +26,10 @@ double prompt_input(char* prompt_message, int (*return_check)(double,int)){
         frac_part = modf(input,&int_part);
         if (return_check(frac_part,input))
             return input;
-        printf("Invalid input. Please Try again.\n");
+        printf("%s","Invalid input. Please Try again.\n");
         prompts_num++;
     };
-    printf("Invalid input for 3 consecutive times - Exiting...\n");
+    printf("%s","Invalid input for 3 consecutive times - Exiting...\n");
     exit(0);
     return 1;
 };
@@ -50,17 +50,17 @@ double cash_deposition(double cur_balance, double money_deposit){
     return cur_balance;
 };
 
-void quit(int transactions){
-    printf("Your number of transactions are %d \nThank you for using the ATM\n",transactions);
+void quit(unsigned int transactions){
+    printf("Your number of transactions are %u \nThank you for using the ATM\n",transactions);
     exit(0);
 };
 
-void menu(int trans_num,int nana_pin,double cur_trans_value,double init_balance,int (*return_check)(double,int)){    
+void menu(int trans_num,int nana_pin,double cur_trans_value,double init_balance,int (*return_check)(double,unsigned int)){    
     return_check = validate_menu;
     char *menu_prompt = "Please enter your command\n\tEnter 1 for cash withdrawal\n\tEnter 2 for cash deposition\n\tEnter 3 to check balance\n\tEnter 4 to exit\n";
-    int choice = (int)prompt_input(menu_prompt,return_check);
+    unsigned int choice = (unsigned int)prompt_input(menu_prompt,return_check);
 
-    while (choice){
+    while (choice != 4){
         if (choice == 1){
             return_check = validate_withrawal;
             init_balance = cash_withdrawal(init_balance,prompt_input("Please enter amount to withdraw: ",return_check));
@@ -73,20 +73,18 @@ void menu(int trans_num,int nana_pin,double cur_trans_value,double init_balance,
             balance(init_balance);
             trans_num--;
         }
-        if (choice == 4)
-            quit(trans_num);
         trans_num ++;
         return_check = validate_menu;
-        choice = (int)prompt_input(menu_prompt,return_check);
+        choice = (unsigned int)prompt_input(menu_prompt,return_check);
     };
+    quit(trans_num);
 };
 
 int main (int argc, char const *argv[]){
     int trans_num = 0;
-    int nana_pin = 3014;
     double cur_trans_value = 0;
     double init_balance = 5000;
-    int (*return_check)(double,int) = validate_pin;
+    unsigned int nana_pin = 3014,(*return_check)(double,unsigned int) = validate_pin;
     prompt_input("Please enter your pin # to continue ",return_check);
     menu(trans_num,nana_pin,cur_trans_value,init_balance,return_check);
     return 0;
