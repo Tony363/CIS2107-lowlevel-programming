2ce49681dcca1458f971d5acb5307bde53a62e4b
tony <thefool363@gmail.com>
2023-09-25 18:58:41 -0400

    why seg fault ?

--- src/Arrays2D.c
+++ src/Arrays2D.c
@@ -8,26 +8,34 @@ int columnSum(int **arr, size_t col_idx, size_t row);
 int isSquare(size_t row, size_t col);
 void displayOutputs(int **arr,size_t row,size_t col);
 
-int get_array(size_t row,size_t col);
+int** get_array(size_t row,size_t col);
 void deallocate_array(int **arr, size_t row);
 
 int main(){
     size_t row, col = 0;
     puts("Let's create a 2Dim array");
-    scanf("How many rows? %zu",&row);
-    scanf("How many columns? %zu",&col);
+    printf("%s","How many rows? ");
+    scanf("%zu",&row);
+    printf("%s","How many columns? ");
+    scanf("%zu",&col);
     int **arr = get_array(row,col);
 
-    printf("Sum of row 1 = %d",rowSum(arr,1,col));
-    printf("Sum of row 2 = %d",rowSum(arr,2,col));
-    puts("");
-    printf("Sum of column 1 = %d",rowSum(arr,1,col));
-    printf("Sum of column 2 = %d",rowSum(arr,2,col));
-    printf("Sum of column 3 = %d",rowSum(arr,3,col));
+    // int sum = rowSum(arr,1,col);
+    // printf("Sum of row 1 = %d",sum);
+    // sum = rowSum(arr,2,col);
+    // printf("Sum of row 2 = %d",sum);
+    // puts("");
+
+    // sum = columnSum(arr,1,col);
+    // printf("Sum of column 1 = %d",sum);
+    // sum = (int)columnSum(arr,2,col);
+    // printf("Sum of column 2 = %d",sum);
+    // sum = (int)columnSum(arr,3,col);
+    // printf("Sum of column 3 = %d",sum);
 
     printf("This is %s array.\n",isSquare(row,col) ? "square" : "not square");
     displayOutputs(arr,row,col);
-    deallocate_array(arr);
+    deallocate_array(arr,row);
     return 0;
 }
 
@@ -39,11 +47,11 @@ int isSquare(size_t row, size_t col){
 void displayOutputs(int **arr,size_t row,size_t col){
     puts("Here is yiour 2Dim array:");
     for (size_t i = 0; i < row; i++){
-        printf("%c","[");
+        printf("%c",'[');
         for (size_t j = 0; j < col; j++){
             printf("%4d,",arr[i][j]);
         }
-        printf("%c\n","}");
+        printf("%c\n",']');
     }
 }
 
@@ -59,8 +67,9 @@ int max(int **arr,size_t row, size_t col){
 
 int rowSum(int **arr,size_t row_idx,size_t col){
     int sum = 0;
-    for (size_t i = 0; i < col; i++)
-        sum += arr[row_idx][i];
+    for (size_t i = 0; i < col; i++){
+        sum = sum + arr[row_idx][i];
+    }
     return sum;
 };
 
@@ -72,15 +81,15 @@ int columnSum(int **arr, size_t col_idx, size_t row){
 }
 
 
-int get_array(size_t row,size_t col){
+int** get_array(size_t row,size_t col){
     int input = 0;
     int **array = (int **)malloc(row * sizeof(int *));
 
     for (size_t i = 0; i < row; i++){
         array[i] = (int *)malloc(col * sizeof(int));
-        for (size_t j = 0; i < col; i++){
-            printf("enter [%d][%d]",i,j);
-            scanf("%d ",&input);
+        for (size_t j = 0; j < col; j++){
+            printf("enter [%lu][%lu] ",i,j);
+            scanf("%d",&input);
             array[i][j] = input;
         }
     }
