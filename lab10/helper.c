555a88e700319b8e1a46017c7b64c9f01326cf00
tony <thefool363@gmail.com>
2023-12-10 02:57:05 -0500

    off by one issues

--- lab10/helper.c
+++ lab10/helper.c
@@ -3,12 +3,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int is_diaglogue(char cur_char){
-    return (cur_char == '"');
-}
-
-
-
 char *_southernize(FILE *infile,long int start_pos,long int end_pos){
     FILE *cur_pos = infile;
     if (start_pos >= end_pos) {
@@ -24,15 +18,6 @@ char *_southernize(FILE *infile,long int start_pos,long int end_pos){
     }
 
     fseek(infile, start_pos, SEEK_SET);
-    // char cur_char = '\0';
-    // while (ftell(infile) < end_pos){
-    //     cur_char = fgetc(infile);
-    //     if (cur_char == '\n')
-    //         continue;
-    //     printf("%c",cur_char);
-        
-    //     buffer[ftell(infile)-start_pos] = cur_char;
-    // }
     if (fread(buffer, 1, length, infile) != length) {
         fprintf(stderr, "Failed to read the file.\n");
         free(buffer);
@@ -43,48 +28,74 @@ char *_southernize(FILE *infile,long int start_pos,long int end_pos){
     return buffer;
 }
 
+void print_queue(char* queue,int char_iter){
+    for (int i = 0; i < 4; i++)
+        printf("%c",queue[(char_iter - 4 + i) % 4]);
+    printf("\n");
+}
+
+int is_diaglogue(char cur_char){
+    return (cur_char == '"');
+}
+
 int is_vowel(char ch){
     ch = tolower(ch);
-    return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    return ch == 'a' 
+        || ch == 'e' 
+        || ch == 'i' 
+        || ch == 'o' 
+        || ch == 'u';
 }
 
-int is_very(char* window){
-    return tolower(window[0]) == 'v' && tolower(window[1]) == 'e' && tolower(window[2]) == 'r' && tolower(window[3]) == 'y';
+int is_very(char* queue, int char_iter){
+    return tolower(queue[(char_iter - 3) % 4]) == 'v' 
+        && tolower(queue[(char_iter - 2) % 4]) == 'e' 
+        && (tolower(queue[(char_iter - 1) % 4]) == 'h' || tolower(queue[(char_iter - 1) % 4]) == 'r')
+        && tolower(queue[char_iter % 4]) == 'y';
 }
 
+int is_yah(char* queue,int char_iter){
+    // print_queue(queue,char_iter);
+    return (tolower(queue[char_iter % 4] == 's' || tolower(queue[char_iter % 4]) == ' ') || tolower(queue[char_iter % 4]) == '.')
+        && (tolower(queue[(char_iter - 1) % 4]) == 'r')// || tolower(queue[(char_iter - 1) % 4]) == 'h')
+        && ((tolower(queue[(char_iter - 3) % 4]) == 'e' && tolower(queue[(char_iter - 2) % 4]) == 'e') || tolower(queue[(char_iter - 2) % 4]) == 'i');
+}
 
+int is_wah(char *queue,int char_iter){
+    return (tolower(queue[char_iter % 4] == 's' || tolower(queue[char_iter % 4]) == ' ') || tolower(queue[char_iter % 4]) == '.')
+        && (tolower(queue[(char_iter - 1) % 4]) == 'r')// || tolower(queue[(char_iter - 1) % 4]) == 'h')
+        && tolower(queue[(char_iter - 3) % 4]) == 'o' 
+        && tolower(queue[(char_iter - 2) % 4]) == 'o';
+}
 
 void southernize(FILE* infile,FILE* outfile){
-    int end = -1;
-    long int start_pos = 0,end_pos = 0;
-    char window[4] = {0};
-    char *buffer = NULL;
-    while ((window[3] = fgetc(infile)) != EOF){
-        end += (int)is_diaglogue(window[1]);
-        if (is_very(window)){
+    int end = -1,char_iter = 0;
+    char queue[4] = {0}; 
+    queue[char_iter++ % 4] = fgetc(infile);
+    while ((queue[char_iter % 4] = fgetc(infile)) != EOF){
+        end += (int)is_diaglogue(queue[char_iter % 4]);
+        if (!(end % 2) && is_very(queue,char_iter)){
             fseek(outfile,-3,SEEK_CUR);
-            fputs("wicked",outfile);
+            fputs("wicked ",outfile);
+            queue[char_iter++ % 4] = fgetc(infile);
+            continue;
         }
-
-        if (!(end % 2) && is_vowel(window[1]) && tolower(window[2]) == 'r')
-            window[1] = 'h';
-        if (!(end % 2) &&  window[3] == ' ' && tolower(window[2]) == 'a')
+        if (!(end % 2) && is_yah(queue,char_iter)){
+            fseek(outfile,-1,SEEK_CUR);
+            fputs("yah",outfile);
+        }
+        if (!(end % 2) && is_wah(queue,char_iter)){
+            fseek(outfile,-1,SEEK_CUR);
+            fputs("wah",outfile);
+        }
+        if (!(end % 2) && is_vowel(queue[(char_iter - 1) % 4]) && tolower(queue[char_iter % 4]) == 'r')
+            queue[char_iter % 4] = 'h';
+        if (!(end % 2) &&  queue[char_iter % 4] == ' ' && tolower(queue[(char_iter - 1) % 4]) == 'a')
             fputc('r',outfile);
-        // if (!start_pos && (end % 2) == 0)
-        //     start_pos = ftell(infile);
-        
-        // if (start_pos && (end % 2) == 1){
-        //     end_pos = ftell(infile);
-        //     buffer = _southernize(infile,start_pos-10,end_pos);
-        //     puts(buffer);
-        //     start_pos = end_pos = 0;
-        // }
-        if (end > 6)
-            break;
-        window[0] = window[1];  
-        window[1] = window[2];
-        window[2] = window[3];
-        fputc(window[2], outfile);
+
+        // if (end > 80)
+        //     break;
+        fputc(queue[char_iter % 4], outfile);
+        char_iter++;
     }
-    fputc(window[3], outfile);
 }
